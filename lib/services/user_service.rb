e282e452c6daf3b95c84b0d8fd62cf4940ccbc24
Jesus.macias <jesus.macias@meliusid.com>
2023-05-14 16:06:35 -0500

    feat: add (create_user)

--- lib/services/user_service.rb
+++ lib/services/user_service.rb
@@ -1,5 +1,6 @@
 class UserService < BaseService
   def create_user(name, email, role)
-    #TODO Logic
+    user_repository = UserRepository.new
+    user_repository.create_user({name: name, email: email, role: role})
   end
 end
