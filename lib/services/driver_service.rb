df60aa38bcc5f797ae480e04c8c0fd8909851bdb
Jesus.macias <jesus.macias@meliusid.com>
2023-05-14 16:46:35 -0500

    feat: add (end_travel)

--- lib/services/driver_service.rb
+++ lib/services/driver_service.rb
@@ -1,6 +1,18 @@
 class DriverService < BaseService
-    def end_travel(travel_id, lat_end, long_end)
-      #TODO Logic
-    end
+  def end_travel(travel_id, lat_end, long_end)
+    travel_repository = TravelRepository.new
+    user_repository = UserRepository.new
+
+    travel = travel_repository.find_by(travel_id)
+    driver = user_repository.find_by(travel[:driver_id])
+    return add_error('Invalid driver') unless driver.present?
+    rider = user_repository.find_by(travel[:rider_id])
+
+    cost = 10
+    travel.update( lat_end: lat_end, long_end: long_end, status: 2, amount: cost )
+    rider.update(status: 0)
+    driver.update(status: 0)
+
+    cost
+  end
 end
-  
